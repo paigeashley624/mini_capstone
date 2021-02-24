688f25b83b997b03f6dee4384052758465073f44
Paige Morton <paigeashley624@gmail.com>
2021-02-24 16:16:11 -0400

    add products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product = Product.create([
+  {name: "Nintendo"},
+  {price: 300},
+  {image_url: "https://images.pexels.com/photos/371924/pexels-photo-371924.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"}
+  {description: "Game system"}
+  ]) 
